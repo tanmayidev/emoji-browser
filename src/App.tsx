856d7e481043d606f2bbb2829ce642856f585724
tanmayidev <tanmayi.d.99@gmail.com>
2023-09-23 13:08:39 +0530

    fetch and show data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,52 @@
-import React from "react";
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
+interface Emoji {
+  name: string;
+  category: string;
+  group: string;
+  htmlCode: string[];
+}
+
 function App() {
-  return <div className="App"></div>;
+  const [emojis, setEmojis] = useState<Emoji[]>([]);
+
+  useEffect(() => {
+    // Fetch emoji details from the API
+    fetch("https://emojihub.yurace.pro/api/all")
+      .then((response) => response.json())
+      .then((data) => {
+        setEmojis(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching emojis:", error);
+        alert("Error fetching emojis");
+      });
+  }, []);
+
+  const getEmojiFromHtmlCode = (htmlCodeArray: string[]): string => {
+    let code: string = "";
+
+    for (const htmlCode of htmlCodeArray) {
+      code += String.fromCodePoint(parseInt(htmlCode.substring(2)));
+    }
+
+    return code;
+  };
+
+  return (
+    <div className="App">
+      {emojis.map((emoji, index) => (
+        <div key={index} className="emoji-card">
+          <div className="emoji">{getEmojiFromHtmlCode(emoji.htmlCode)}</div>
+          <div className="name">{emoji.name}</div>
+          <div className="category">{emoji.category}</div>
+          <div className="group">Group : {emoji.group}</div>
+          <div className="htmlCode">HTML Code : {emoji.htmlCode}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default App;
